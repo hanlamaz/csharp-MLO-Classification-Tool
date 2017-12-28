025f0712556dfa49d38ecebe2c16c6c9a13a731a
Hüseyin Anlamaz <hanlamaz17@gmail.com>
2017-12-28 19:24:59 +0300

    Added dynamic elements , and make tests for trained data.

--- csharp-MLO-Classification-Tool/Form1.Designer.cs
+++ csharp-MLO-Classification-Tool/Form1.Designer.cs
@@ -32,12 +32,15 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Calibri Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textBox1.Location = new System.Drawing.Point(12, 58);
+            this.textBox1.Location = new System.Drawing.Point(12, 80);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(494, 33);
@@ -46,7 +49,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Calibri Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(532, 57);
+            this.button1.Location = new System.Drawing.Point(532, 79);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(125, 34);
             this.button1.TabIndex = 1;
@@ -58,7 +61,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(8, 109);
+            this.label1.Location = new System.Drawing.Point(542, 134);
             this.label1.Margin = new System.Windows.Forms.Padding(3);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(56, 23);
@@ -76,11 +79,48 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "STATUS : NOT WORKING";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(8, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(184, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "TRAIN DATA LOCATION";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 134);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(166, 173);
+            this.flowLayoutPanel1.TabIndex = 10;
+            this.flowLayoutPanel1.WrapContents = false;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Calibri Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(676, 79);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 34);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Push New Instance";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(669, 162);
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1143, 370);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
@@ -99,6 +139,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- csharp-MLO-Classification-Tool/Form1.cs
+++ csharp-MLO-Classification-Tool/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using weka.classifiers;
+using weka.classifiers.evaluation;
+using weka.core;
+using weka.core.neighboursearch;
 
 namespace csharp_MLO_Classification_Tool
 {
@@ -17,31 +21,91 @@ namespace csharp_MLO_Classification_Tool
             InitializeComponent();
 
         }
-
+        weka.core.Instances insts;
+        weka.core.Instances instsTest;
+        public string testLoc;
+        List<trainedData> myData = new List<trainedData>();
+        Instance ins;
         const int percentSplit = 66;
-        public async Task<double> classifyTest(weka.classifiers.Classifier cl, string file)
+        private async Task loadFileAndMakeElements(string location)
         {
-            
+            if (location.EndsWith(".csv"))
+            {
+                weka.core.converters.CSVLoader csvLoader = new weka.core.converters.CSVLoader();
+                csvLoader.setSource(new java.io.File(location));
+                insts = csvLoader.getDataSet();
+                insts.setClassIndex(insts.numAttributes() - 1);
+            }
+            else
+            {
+                insts = new weka.core.Instances(new java.io.FileReader(location));
+                insts.setClassIndex(insts.numAttributes() - 1);
+            }
+            flowLayoutPanel1.Controls.Clear();
+            for (int i = 0; i < insts.numAttributes() - 1; i++)
+            {
+                if (insts.attribute(i).isNominal() == true)
+                {
+                    if (insts.attribute(i).numValues() > 0)
+                    {
+                        Label lbl = new Label();
+                        lbl.Text = insts.attribute(i).name().Trim();
+                        lbl.Enabled = true;
+                        ComboBox cmbBox = new ComboBox();
+                        cmbBox.Name = insts.attribute(i).name();
+                        for (int m = 0; m < insts.attribute(i).numValues(); m++)
+                        {
+                            cmbBox.Items.Add(insts.attribute(i).value(m));
+                        }
+                        cmbBox.DropDownStyle = ComboBoxStyle.DropDownList;
+                        cmbBox.Enabled = true;
+                        flowLayoutPanel1.Controls.Add(lbl);
+                        flowLayoutPanel1.Controls.Add(cmbBox);
+                    }
+                    else
+                    {
+
+                    }
+
+                }
+                else if (insts.attribute(i).isNumeric() == true)
+                {
+                    Label lbl = new Label();
+                    lbl.Text = insts.attribute(i).name().Trim();
+                    TextBox txtBox = new TextBox();
+                    txtBox.Name = insts.attribute(i).name();
+                    txtBox.KeyPress += new KeyPressEventHandler(txtBox_Keypress);
+                    flowLayoutPanel1.Controls.Add(lbl);
+                    flowLayoutPanel1.Controls.Add(txtBox);
+                }
+            }
+        }
+        private void txtBox_Keypress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+        public async Task<string> classifyTest(weka.classifiers.Classifier cl)
+        {
+            string a = "";
             double rate = 0;
             try
             {
-                weka.core.Instances insts;
+                //instsTest = Instances.mergeInstances(ins,null);
+                
+                /*if (ins.classIndex() == -1)
+                    ins.setClassIndex(insts.numAttributes() - 1);*/
 
-                if (file.EndsWith(".csv"))
-                {
-                    weka.core.converters.CSVLoader csvLoader = new weka.core.converters.CSVLoader();
-                    csvLoader.setSource(new java.io.File(file));
-                    insts = csvLoader.getDataSet();
-                    insts.setClassIndex(insts.numAttributes() - 1);
-                }
-                else
-                {
-                    insts = new weka.core.Instances(new java.io.FileReader(file));
-                    insts.setClassIndex(insts.numAttributes() - 1);
-                }
-
-
-                Console.WriteLine("Performing " + percentSplit + "% split evaluation.");
+                System.Console.WriteLine("Performing " + percentSplit + "% split evaluation.");
 
                 weka.filters.Filter normalized = new weka.filters.unsupervised.attribute.Normalize();
                 normalized.setInputFormat(insts);
@@ -57,13 +121,20 @@ namespace csharp_MLO_Classification_Tool
                 replaceMissingValues.setInputFormat(insts);
                 insts = weka.filters.Filter.useFilter(insts, replaceMissingValues);
 
+
                 int trainSize = insts.numInstances() * percentSplit / 100;
                 int testSize = insts.numInstances() - trainSize;
                 weka.core.Instances train = new weka.core.Instances(insts, 0, trainSize);
 
                 cl.buildClassifier(train);
 
+               //double label = cl.classifyInstance(instsTest.instance(0));
+                double label = cl.classifyInstance(ins);
+                ins.setClassValue(label);
+                //instsTest.instance(0).setClassValue(label);
+                a = ins.toString(ins.numAttributes()-1);
 
+                weka.core.SerializationHelper.write("mymodel.model", cl);
                 int numCorrect = 0;
                 for (int i = trainSize; i < insts.numInstances(); i++)
                 {
@@ -78,88 +149,76 @@ namespace csharp_MLO_Classification_Tool
             }
             catch (java.lang.Exception ex)
             {
-                ex.printStackTrace();
+                //ex.printStackTrace();
                 rate = -1;
             }
-
-            return rate;
+            return rate.ToString() + ";" + a??"";
         }
 
-        private async void button1_Click(object sender, EventArgs e)
+        private async Task makeTests()
         {
             string infoStr = "";
-            label1.Text = "";
-            Dictionary<string, double> rateDic = new Dictionary<string, double>(); // A dictionary that contains algorithm and its result.
-            OpenFileDialog file = new OpenFileDialog();
-            file.Title = "Open CSV or ARFF";
-            file.InitialDirectory = "C:\\";
-            file.Filter = "Attribute-Relation File Format |*.arff|Comma-separated values |*.csv";
-            if (file.ShowDialog() == DialogResult.OK)
-            {
-                textBox1.Text = file.FileName;
-                label2.Text = "STATUS = STARTED";
-                await Task.Delay(50);
-            }
+            Dictionary<string, string> rateDic = new Dictionary<string, string>(); // A dictionary that contains algorithm and its result.
             await Task.Delay(50);// For show file location in textbox
-            rateDic.Add("J48", await classifyTest(new weka.classifiers.trees.J48(), file.FileName));
+            rateDic.Add("J48", await classifyTest(new weka.classifiers.trees.J48()));
             label2.Text = "STATUS = Working on " + rateDic.Keys.ElementAt(rateDic.Keys.Count() - 1);
             await Task.Delay(50);
-            rateDic.Add("Naive Bayes", await classifyTest(new weka.classifiers.bayes.NaiveBayes(), file.FileName));
+            rateDic.Add("Naive Bayes", await classifyTest(new weka.classifiers.bayes.NaiveBayes()));
             label2.Text = "STATUS = Working on " + rateDic.Keys.ElementAt(rateDic.Keys.Count() - 1);
             await Task.Delay(50);
-            rateDic.Add("1-IBk", await classifyTest(new weka.classifiers.lazy.IBk(1), file.FileName));
+            rateDic.Add("1-IBk", await classifyTest(new weka.classifiers.lazy.IBk(1)));
             label2.Text = "STATUS = Working on " + rateDic.Keys.ElementAt(rateDic.Keys.Count() - 1);
             await Task.Delay(50);
-            rateDic.Add("3-IBk", await classifyTest(new weka.classifiers.lazy.IBk(3), file.FileName));
+            rateDic.Add("3-IBk", await classifyTest(new weka.classifiers.lazy.IBk(3)));
             label2.Text = "STATUS = Working on " + rateDic.Keys.ElementAt(rateDic.Keys.Count() - 1);
             await Task.Delay(50);
-            rateDic.Add("5-IBk", await classifyTest(new weka.classifiers.lazy.IBk(5), file.FileName));
+            rateDic.Add("5-IBk", await classifyTest(new weka.classifiers.lazy.IBk(5)));
             label2.Text = "STATUS = Working on " + rateDic.Keys.ElementAt(rateDic.Keys.Count() - 1);
             await Task.Delay(50);
-            rateDic.Add("7-IBk", await classifyTest(new weka.classifiers.lazy.IBk(7), file.FileName));
+            rateDic.Add("7-IBk", await classifyTest(new weka.classifiers.lazy.IBk(7)));
             label2.Text = "STATUS = Working on " + rateDic.Keys.ElementAt(rateDic.Keys.Count() - 1);
             await Task.Delay(50);
-            rateDic.Add("9-IBk", await classifyTest(new weka.classifiers.lazy.IBk(9), file.FileName));
+            rateDic.Add("9-IBk", await classifyTest(new weka.classifiers.lazy.IBk(9)));
             label2.Text = "STATUS = Working on " + rateDic.Keys.ElementAt(rateDic.Keys.Count() - 1);
             await Task.Delay(50);
-            rateDic.Add("Random Forest", await classifyTest(new weka.classifiers.trees.RandomForest(), file.FileName));
+            rateDic.Add("Random Forest", await classifyTest(new weka.classifiers.trees.RandomForest()));
             label2.Text = "STATUS = Working on " + rateDic.Keys.ElementAt(rateDic.Keys.Count() - 1);
             await Task.Delay(50);
-            rateDic.Add("Random Tree", await classifyTest(new weka.classifiers.trees.RandomTree(), file.FileName));
+            rateDic.Add("Random Tree", await classifyTest(new weka.classifiers.trees.RandomTree()));
             label2.Text = "STATUS = Working on " + rateDic.Keys.ElementAt(rateDic.Keys.Count() - 1);
             await Task.Delay(50);
-            rateDic.Add("REPTree", await classifyTest(new weka.classifiers.trees.REPTree(), file.FileName));
+            rateDic.Add("REPTree", await classifyTest(new weka.classifiers.trees.REPTree()));
             await Task.Delay(50);
             label2.Text = "STATUS = Working on " + rateDic.Keys.ElementAt(rateDic.Keys.Count() - 1);
-            rateDic.Add("LMT", await classifyTest(new weka.classifiers.trees.LMT(), file.FileName));
+            rateDic.Add("LMT", await classifyTest(new weka.classifiers.trees.LMT()));
             label2.Text = "STATUS = Working on " + rateDic.Keys.ElementAt(rateDic.Keys.Count() - 1);
             await Task.Delay(50);
-            rateDic.Add("Multilayer Perceptron", await classifyTest(new weka.classifiers.functions.MultilayerPerceptron(), file.FileName));
+            rateDic.Add("Multilayer Perceptron", await classifyTest(new weka.classifiers.functions.MultilayerPerceptron()));
             label2.Text = "STATUS = Working on " + rateDic.Keys.ElementAt(rateDic.Keys.Count() - 1);
             await Task.Delay(50);
-            rateDic.Add("SMO", await classifyTest(new weka.classifiers.functions.SMO(), file.FileName));
+            rateDic.Add("SMO", await classifyTest(new weka.classifiers.functions.SMO()));
             label2.Text = "STATUS = Working on " + rateDic.Keys.ElementAt(rateDic.Keys.Count() - 1);
             await Task.Delay(50);
-            rateDic.Add("Logistic Regression", await classifyTest(new weka.classifiers.functions.Logistic(), file.FileName));
+            rateDic.Add("Logistic Regression", await classifyTest(new weka.classifiers.functions.Logistic()));
             label2.Text = "STATUS = Working on " + rateDic.Keys.ElementAt(rateDic.Keys.Count() - 1);
             await Task.Delay(50);
-            rateDic.Add("Linear Regression", await classifyTest(new weka.classifiers.functions.LinearRegression(), file.FileName));
+            rateDic.Add("Linear Regression", await classifyTest(new weka.classifiers.functions.LinearRegression()));
             label2.Text = "STATUS = Working on " + rateDic.Keys.ElementAt(rateDic.Keys.Count() - 1);
             await Task.Delay(50);
-            var ordered = rateDic.OrderByDescending(x => x.Value);
+            var ordered = rateDic.OrderByDescending(x => double.Parse(x.Value.Split(';')[0]));
             label2.Text = "STATUS = Working on  ORDERING";
             await Task.Delay(50);
             int counter = 1;
             foreach (var item in ordered)
             {
-                if (item.Value == -1)
+                if (item.Value.Split(';')[0].ToString() == "-1")
                 {
-                    infoStr += counter + ") " + item.Key + " is unavaible for this" + Environment.NewLine;
+                    infoStr += counter + ") " + item.Key + " is unavaible for this" + System.Environment.NewLine;
                     counter++;
                 }
                 else
                 {
-                    infoStr += counter + ") " + item.Key + " with %" + item.Value + " success rate" + Environment.NewLine;
+                    infoStr += counter + ") " + item.Key + " with %" + item.Value.Split(';')[0] + " success rate , and Test Result is : " + item.Value.Split(';')[1] + System.Environment.NewLine;
                     counter++;
                 }
             }
@@ -167,7 +226,22 @@ namespace csharp_MLO_Classification_Tool
             await Task.Delay(50);
             label1.Text = infoStr;
             label1.Visible = true;
+        }
 
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";     
+            OpenFileDialog file = new OpenFileDialog();
+            file.Title = "Open CSV or ARFF";
+            file.InitialDirectory = "C:\\";
+            file.Filter = "Attribute-Relation File Format |*.arff|Comma-separated values |*.csv";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = file.FileName;
+                await Task.Delay(50);
+            }
+            button3.Enabled = true;
+            await loadFileAndMakeElements(file.FileName);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -175,6 +249,95 @@ namespace csharp_MLO_Classification_Tool
             this.AutoSize = true;
             this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
             this.Dock = DockStyle.Fill;
+            button3.Enabled = false;
+        }
+
+
+        public async Task makeNewInstance()
+        {
+            int a = 0;
+            ins = new DenseInstance(insts.numAttributes());
+            ins.setDataset(insts);
+            foreach (trainedData item in myData)
+            {
+                if (item.isNumber==false)
+                {
+                    weka.core.Attribute m = insts.attribute(item.name);
+                    ins.setValue(m, item.value);
+                    a++;
+                }
+                else
+                {
+
+                }
+            }
+        }
+        public async Task getInsertedElements()
+        {
+            List<String> values = new List<String>();
+            foreach (Control c in flowLayoutPanel1.Controls.OfType<Control>())
+                if (string.IsNullOrEmpty(c.Name) && string.IsNullOrEmpty(c.Text))
+                {
+                    MessageBox.Show("Please fill all info");
+                    break;
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(c.Text))
+                    {
+                        values.Add(c.Name);
+                    }
+                    else
+                    {
+                        var a = (c as ComboBox);
+                        if (a!=null)
+                        {
+                            values.Add((c as ComboBox).SelectedItem.ToString());
+                        }
+                        else
+                        {
+                            values.Add(c.Text);
+                        }
+                        
+                    }
+                    
+                }
+            for (int i = 0; i < values.Count; i += 2)
+            {
+                trainedData trn = new trainedData();
+                double price;
+                bool isDouble = Double.TryParse(values[i + 1], out price);
+                if (isDouble)
+                {
+                    trn.name = values[i];
+                    trn.value = values[i + 1];
+                    trn.isNumber = true;
+                }
+                else
+                {
+                    trn.name = values[i];
+                    trn.value = values[i + 1];
+                    trn.isNumber = false;
+                }
+                myData.Add(trn);
+            }
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            label2.Text = "STATUS = STARTED";
+            await Task.Delay(50);
+            await getInsertedElements();
+            await Task.Delay(50);
+            await makeNewInstance();
+            await Task.Delay(50);
+            await makeTests();
+        }
+        public class trainedData
+        {
+            public string name { get; set; }
+            public string value { get; set; }
+            public bool isNumber { get; set; }
         }
     }
 }
